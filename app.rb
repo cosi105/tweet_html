dff31d5ed69a79e50148be134f209dc4e8f088c9
Bradford Nesbitt <bradfordnesbitt@Bradfords-MacBook-Pro.local>
2019-04-24 15:41:57 -0400

    Supports even/odd Redis shards & tweet HTML generation.

--- app.rb
+++ app.rb
@@ -6,8 +6,10 @@ set :port, 8082 unless Sinatra::Base.production?
 
 if Sinatra::Base.production?
   configure do
-    redis_uri = URI.parse(ENV['REDISCLOUD_URL'])
-    REDIS = Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password)
+    redis_even_uri = URI.parse(ENV['REDIS_URL'])
+    redis_odd_uri = URI.parse(ENV['HEROKU_REDIS_PURPLE_URL'])
+    REDIS_EVEN = Redis.new(host: redis_even_uri.host, port: redis_even_uri.port, password: redis_even_uri.password)
+    REDIS_ODD = Redis.new(host: redis_odd_uri.host, port: redis_odd_uri.port, password: redis_odd_uri.password)
   end
   rabbit = Bunny.new(ENV['CLOUDAMQP_URL'])
 else
@@ -19,38 +21,40 @@ end
 rabbit.start
 channel = rabbit.create_channel
 RABBIT_EXCHANGE = channel.default_exchange
-
-NEW_TWEET = channel.queue('new_tweet.tweet_data')
-FOLLOWER_IDS = channel.queue('new_tweet.follower_ids')
+new_tweet = channel.queue('new_tweet.tweet_data')
+follower_ids = channel.queue('new_tweet.follower_ids')
 HTML_FANOUT = channel.queue('new_tweet.html_fanout')
-EVEN = channel.queue('new_tweet.tweet_html.even')
-ODD = channel.queue('new_tweet.tweet_html.odd')
 
-NEW_TWEET.subscribe(block: false) do |delivery_info, properties, body|
-  route_tweet(JSON.parse(body))
+# Takes a new_tweet payload, generates the tweet's html & caches it
+new_tweet.subscribe(block: false) do |delivery_info, properties, body|
+  tweet_json = JSON.parse(body)
+  tweet_id = tweet_json['tweet_id'].to_i
+  tweet_html = render_html(tweet_json)
+  redis_shard = tweet_id.even? ? REDIS_EVEN : REDIS_ODD # Thread safe?
+  redis_shard.set(tweet_id, tweet_html)
 end
 
-FOLLOWER_IDS.subscribe(block: false) do |delivery_info, properties, body|
+# Generates a payload containing a new tweet's HTML & the IDs of followers
+# who need to have the new Tweet added to their timeline HTML.
+follower_ids.subscribe(block: false) do |delivery_info, properties, body|
   fanout_to_html(JSON.parse(body))
 end
 
-def route_tweet(body)
-  tweet_id = body['tweet_id'].to_i
-  shard = tweet_id.even? ? EVEN : ODD
-  RABBIT_EXCHANGE.publish(body, routing_key: shard.name)
+# Generates & returns a Tweet's (timeline piece) HTML
+def render_html(body)
+  tweet_body = body['tweet_body']
+  tweet_created = body['tweet_created']
+  author_handle = body['author_handle']
+  "<li>#{tweet_body}<br>- #{author_handle} #{tweet_created}</li>"
 end
 
+# Fetches a Tweet's HTML from the appropriate Redis shard, then
+# enqueues it in a payload with the IDs of the Tweet's followers.
 def fanout_to_html(body)
   tweet_id = body['tweet_id'].to_i
-  shard = tweet_id.even? ? 'EVEN_URL' : 'ODD_URL'
-  shard = ENV[shard]
-  get_resp = nil
-  loop do
-    get_resp = HTTParty.post "#{shard}/get_tweet/#{tweet_id}"
-    break #TODO: unless get_resp fails
-  end
+  redis_shard = tweet_id.even? ? REDIS_EVEN : REDIS_ODD # Thread safe?
   payload = {
-    tweet_html: get_resp, #TODO: get body of get_resp
+    tweet_html: redis_shard.get(tweet_id),
     user_ids: body['follower_ids']
   }
   RABBIT_EXCHANGE.publish(payload, routing_key: HTML_FANOUT.name)
